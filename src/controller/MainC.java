1c38441844323a2cce0bf35e675e7e96bcc6fe9a
sawyersago <ce.mustafasahin@gmail.com>
2014-03-02 13:31:41 +0200

    maincont

---
+++ src/controller/MainC.java
@@ -0,0 +1,62 @@
+package controller;
+
+import java.util.ArrayList;
+
+import controller.bilgisayar.BilgisayarC;
+import controller.kisi.KisiC;
+import controller.sira.SiraC;
+import controller.urun.UrunC;
+
+public class MainC {
+    
+    private KisiC kisiCont;
+    private UrunC urunCont;
+
+    private SiraC siraCont;
+
+    private BilgisayarC bilgisayarC;
+
+
+    public MainC(){        
+        kisiCont = new KisiC();
+        urunCont = new UrunC();
+
+        siraCont = new SiraC();
+
+        bilgisayarC = new BilgisayarC();
+
+    }
+    
+    
+    
+    
+    
+    /****************************************************/
+
+    public KisiC getKisiCont() {
+        return kisiCont;
+    }
+
+    public UrunC getUrunCont() {
+        return urunCont;
+    }
+
+	public SiraC getSiraCont() {
+		return siraCont;
+	}
+
+    public void setUrunCont(UrunC urunCont) {
+		this.urunCont = urunCont;
+	}
+    
+	public BilgisayarC getBilgisayarC() {
+		return bilgisayarC;
+	}
+
+	public void setBilgisayarC(BilgisayarC bilgisayarC) {
+		this.bilgisayarC = bilgisayarC;
+	}  
+
+    
+
+}
